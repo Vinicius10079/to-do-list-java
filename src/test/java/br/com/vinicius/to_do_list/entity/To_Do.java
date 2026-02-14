c4ad378b6ab65e1f5dfa62ca4a43a4ac39feed14
Vinicius Reis <vinicius10079@hotmail.com>
2026-02-14 20:34:15 -0300

    feat: criação da entidade ToDo

---
+++ src/test/java/br/com/vinicius/to_do_list/entity/To_Do.java
@@ -0,0 +1,58 @@
+package br.com.vinicius.to_do_list.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "to_dos")
+public class To_Do {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean concluido;
+    private int prioridade;
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isConcluido() {
+        return concluido;
+    }
+    public void setConcluido(boolean concluido) {
+        this.concluido = concluido;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    
+}
